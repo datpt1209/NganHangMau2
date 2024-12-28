1c2e907efcedb3c2fec3c7460c315c6e407e8e2f
Phan Dat <datpt1209@gmail.com>
2024-12-28 22:46:51 +0700

    thêm hồ sơ kiểm tra nhóm máu

--- NganHangMau2/ReportWindow.xaml.cs
+++ NganHangMau2/ReportWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Forms.Integration;
 
 namespace NganHangMau2
 {
@@ -18,77 +19,29 @@ namespace NganHangMau2
         {
             InitializeComponent();
             this.bloodBags = bloodBags;
+            this.Loaded += Window_Loaded;
         }
-
-        //private void Window_Loaded(object sender, RoutedEventArgs e)
-        //{
-        //    // Group data by BloodGroup
-        //    groupedData = bloodBags
-        //        .GroupBy(b => b.BloodGroup)
-        //        .ToDictionary(g => g.Key, g => g.Select(b => new SubReportData
-        //        {
-        //            BloodGroup = b.BloodGroup,
-        //            Id = b.Id,
-        //            ExpiryDate = b.ExpiryDate,
-        //            ProductionDate = b.ProductionDate,
-        //            BloodProductType = b.BloodProductType,
-        //            EnteredBy = b.EnteredBy
-        //        }).ToList());
-
-        //    // Create a list of unique blood groups for the main report
-        //    var reportSource = groupedData.Keys.Select(bloodGroup => new { BloodGroup = bloodGroup }).ToList();
-        //    reportViewer.LocalReport.ReportEmbeddedResource = "NganHangMau2.BloodStorageReport.rdlc";
-
-        //    ReportDataSource rds = new ReportDataSource();
-        //    rds.Name = "DataSet1";
-        //    rds.Value = reportSource;
-        //    reportViewer.LocalReport.DataSources.Clear();
-        //    reportViewer.LocalReport.DataSources.Add(rds);
-
-        //    // Add SubreportProcessing event handler
-        //    reportViewer.LocalReport.SubreportProcessing += new SubreportProcessingEventHandler(LocalReport_SubreportProcessing);
-
-
-        //    // Set display mode to Print Preview
-        //    reportViewer.SetDisplayMode(DisplayMode.PrintLayout);
-
-
-        //    reportViewer.RefreshReport();
-        //}
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // Group data by BloodGroup
-            groupedData = bloodBags
-                .GroupBy(b => b.BloodGroup)
-                .ToDictionary(g => g.Key, g => g.Select(b => new SubReportData
-                {
-                    BloodGroup = b.BloodGroup,
-                    Id = b.Id,
-                    ExpiryDate = b.ExpiryDate,
-                    ProductionDate = b.ProductionDate,
-                    BloodProductType = b.BloodProductType,
-                    EnteredBy = b.EnteredBy
-                }).ToList());
+            // Initialize the first WindowsFormsHost
+            ReportViewer reportViewer1 = new ReportViewer();
+            reportViewer1.LocalReport.ReportEmbeddedResource = "NganHangMau2.BloodStorageReport2.rdlc";
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", bloodBags));
+            reportViewer1.SetDisplayMode(DisplayMode.PrintLayout);
+            reportViewer1.RefreshReport();
+            windowsFormsHost1.Child = reportViewer1;
 
-            // Create a list of unique blood groups for the main report
-            var reportSource = groupedData.Keys.Select(bloodGroup => new { BloodGroup = bloodGroup }).ToList();
-            reportViewer.LocalReport.ReportEmbeddedResource = "NganHangMau2.BloodStorageReport2.rdlc";
+            // Initialize the second WindowsFormsHost
+            ReportViewer reportViewer2 = new ReportViewer();
+            reportViewer2.LocalReport.ReportEmbeddedResource = "NganHangMau2.BloodStorage_BloodGroup.rdlc";
+            reportViewer2.LocalReport.DataSources.Clear();
+            reportViewer2.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", bloodBags));
+            reportViewer2.SetDisplayMode(DisplayMode.PrintLayout);
+            reportViewer2.RefreshReport();
+            windowsFormsHost2.Child = reportViewer2;
 
-            ReportDataSource rds = new ReportDataSource();
-            rds.Name = "DataSet1";
-            rds.Value = bloodBags;
-            reportViewer.LocalReport.DataSources.Clear();
-            reportViewer.LocalReport.DataSources.Add(rds);
-
-            // Add SubreportProcessing event handler
-            reportViewer.LocalReport.SubreportProcessing += new SubreportProcessingEventHandler(LocalReport_SubreportProcessing);
-
-
-            // Set display mode to Print Preview
-            reportViewer.SetDisplayMode(DisplayMode.PrintLayout);
-
-
-            reportViewer.RefreshReport();
+            radioButton1.IsChecked = true;
         }
 
         void LocalReport_SubreportProcessing(object sender, SubreportProcessingEventArgs e)
@@ -98,6 +51,32 @@ namespace NganHangMau2
             if (groupedData.TryGetValue(bloodGroup, out var subReportData))
             {
                 e.DataSources.Add(new ReportDataSource("SubReportDataSet", subReportData));
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (radioButton1.IsChecked == true)
+            {
+                if (windowsFormsHost1 != null)
+                {
+                    windowsFormsHost1.Visibility = Visibility.Visible;
+                }
+                if (windowsFormsHost2 != null)
+                {
+                    windowsFormsHost2.Visibility = Visibility.Collapsed;
+                }
+            }
+            else if (radioButton2.IsChecked == true)
+            {
+                if (windowsFormsHost1 != null)
+                {
+                    windowsFormsHost1.Visibility = Visibility.Collapsed;
+                }
+                if (windowsFormsHost2 != null)
+                {
+                    windowsFormsHost2.Visibility = Visibility.Visible;
+                }
             }
         }
     }
